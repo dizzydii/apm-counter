c503b19751efc2cca3966fb11776cb330801c6c8
DizzyDi <emailed15@gmail.com>
2022-06-02 14:34:13 +0200

    Stable version 1.0

--- apm_sma.py
+++ apm_sma.py
@@ -3,68 +3,84 @@ from pynput.keyboard import Listener, Key
 from pynput.mouse import Listener as MouseListener
 
 
-def handle_click():
-        global running, input_list, list_lengh, previous_press, number_of_total_button_presses, correction_factor
-        current_press = time.perf_counter_ns()
-        time_since_last_press = (current_press - previous_press) / (10**9)
-        input_list[number_of_total_button_presses %
-                list_lengh] = time_since_last_press
-        number_of_total_button_presses += 1
-        previous_press = current_press
-        correction_factor = 1
-        # print(f'{key} was pressed. After {time_since_last_press:.3f} idle time.\nNumber of Presses = {number_of_total_button_presses}')
-        # print(str(input_list))
-        print("APM = ", 60/(sum(input_list)/list_lengh), end='\r')
+def handle_click(idle=None, last_press=None, apm_sma=None):
+        if not apm_sma:
+            return "No Object was given"
+
+        if not idle:
+            apm_sma.current_press = time.perf_counter_ns()
+            apm_sma.time_since_last_press = (apm_sma.current_press - apm_sma.previous_press) / (10**9)
+            apm_sma.time_diff_tracked_inputs_list[apm_sma.number_of_tracked_events %
+                    apm_sma.number_of_tracked_inputs] = apm_sma.time_since_last_press
+            
+            apm_sma.number_of_total_button_presses +=1
+            apm_sma.number_of_tracked_events += 1
+            apm_sma.previous_press = apm_sma.current_press
+            apm_sma.correction_factor = 1
+        else:
+            apm_sma.previous_press = time.perf_counter_ns()
+            apm_sma.time_diff_tracked_inputs_list[apm_sma.number_of_tracked_events %
+                    apm_sma.number_of_tracked_inputs] = 3 * apm_sma.correction_factor
+            apm_sma.correction_factor *= 1.5
+            apm_sma.number_of_tracked_events += 1
+            print(str(apm_sma.time_diff_tracked_inputs_list))
+        return (60/(sum(apm_sma.time_diff_tracked_inputs_list)/apm_sma.number_of_tracked_inputs))    
+        print("APM = ", 60/(sum(apm_sma.time_diff_tracked_inputs_list)/apm_sma.number_of_tracked_inputs), end='\r')
 
 
-def on_release(key):
-    global listener
-    if(key == Key.backspace):
-        listener.stop()
-        print(f"You clicked a total of {number_of_total_button_presses} times")
-    handle_click()
+class apm_sma():
+    def on_press(self, key):
+        if key in self.COMBINATION:
+            print(key)
+            self.current.add(key)
+            if all(k in self.current for k in self.COMBINATION):
+                #print('All modifiers active!')
+                self.listener.stop() # terminate listener and program
 
 
-def on_click(x, y, button, pressed):
-    if(pressed):
-        # print("clicked mouse key " )
-        handle_click()
+    def on_release(self, key):
+        self.apm = handle_click(apm_sma=self)
+        try:
+            self.current.remove(key)
+        except KeyError:
+            pass
+           
 
+    def on_click(self, x, y, button, pressed):
+        if(pressed):
+            #print("clicked mouse key " )
+            return
+        self.apm = handle_click(apm_sma=self)
 
-running = True
-delay = 0.25
-list_lengh = 10
-input_list = [0] * list_lengh
+    
+    def idle(self):
+        last_press = (time.perf_counter_ns() - self.previous_press) / (10**9)
+        if(last_press > 1):
+            if(self.apm > 10):
+                self.apm = handle_click(True, last_press, apm_sma=self)
+            else:
+                self.apm = 0
+              
 
-number_of_total_button_presses = 0
-previous_press = time.process_time_ns()
+    def __init__(self, number_of_tracked_inputs=12, apm_label=None) -> None:        
+        self.COMBINATION = {Key.backspace, Key.ctrl_l}
+        self.current = set()
 
-mouseListener = MouseListener(on_click=on_click)
-listener = Listener(on_release=on_release)
-mouseListener.start()
-listener.start()
-correction_factor = 1
+        self.number_of_tracked_inputs = number_of_tracked_inputs
+        self.time_diff_tracked_inputs_list = [0] * number_of_tracked_inputs
 
+        self.number_of_total_button_presses = 0
+        self.number_of_tracked_events = 0
+        
+        self.previous_press = time.process_time_ns()            
+        self.correction_factor = 1                              # correction_factor to increase the apm_penalty for being idle each cycle 
 
+        self.listener = Listener(on_release=self.on_release, on_press=self.on_press)
+        self.mouseListener = MouseListener(on_click=self.on_click)
+        self.mouseListener.start()
+        self.listener.start()
+        
+        
+        self.label = apm_label
 
-while(listener.running):
-    time.sleep(.25)
-    print(listener.running)
-    last_press = (time.perf_counter_ns() - previous_press) / (10**9)
-
-    if(last_press > 1):
-        # print("hi ", last_press)
-        previous_press = time.perf_counter_ns()
-        input_list[number_of_total_button_presses %
-                list_lengh] = 10 * correction_factor
-        # print(input_list[number_of_total_button_presses % list_lengh])
-        correction_factor *= 1.5
-        number_of_total_button_presses += 1
-        # print(str(input_list), end='\r')
-        print("APM = ", 60/(sum(input_list)/list_lengh), end='\r')
-        # if label:
-        #     label.update()
-
-print("Finished!")
-
-
+        self.apm = 0
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from tkinter import Button
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+import PySimpleGUI as sg
 
-app = QApplication([])
+from apm_sma import apm_sma
 
-win = QMainWindow()
-button = QPushButton("Test")
-win.setCentralWidget(button)
-win.show()
+sg.theme('SystemDefault')
+layout =    [
+    #[sg.Push(), sg.Button("Exit", button_color = ("#AAAA00", "#00AAAA"), key = "-EXIT-")],
+    [sg.VPush()],
+    [sg.Text("0 APM", key = "-APM-", font=("Helvetica", 25))],
+    [sg.VPush()]]
 
-app.exec()
+window = sg.Window('APM Display', 
+        layout,
+        size = (300,80),
+        no_titlebar = False,
+        element_justification = 'right')
+
+apm_object = apm_sma()
+
+while True:
+    event, values = window.read(timeout_key="-TIMEOUT_KEY-", timeout=1000)
+    window['-APM-'].update(value=str(round(apm_object.apm))+ " APM")
+    
+    if event == "-TIMEOUT_KEY-":
+        apm_object.idle()
+        window['-APM-'].update(value=str(round(apm_object.apm))+ " APM")
+
+    if((event == "-EXIT-") | (not apm_object.listener.running) | (event == sg.WIN_CLOSED)):
+        break
+
+window.close()
